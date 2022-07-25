e453ae6ea4e4f11cd3605a00e8cb5082a8537796
CHOIJIHO <wlghsp@gmail.com>
2022-07-25 15:15:27 +0900

    14 수정 폼 만들기 까지

--- src/main/java/com/example/hongproject/controller/ArticleController.java
+++ src/main/java/com/example/hongproject/controller/ArticleController.java
@@ -1,13 +1,25 @@
 package com.example.hongproject.controller;
 
 import com.example.hongproject.dto.ArticleForm;
+import com.example.hongproject.entity.Article;
+import com.example.hongproject.repository.ArticleRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+import java.util.Optional;
+
 @Controller
+@Slf4j // 로깅을 위한 어노테이션
 public class ArticleController {
 
+    @Autowired // 스프링부트가 미리 생성해놓은 객체를 가져다가 자동 연결!
+    private ArticleRepository articleRepository;
     @GetMapping("/articles/new")
     public String newArticleForm() {
         return "articles/new";
@@ -15,8 +27,55 @@ public class ArticleController {
 
     @PostMapping("/articles/create")
     public String createArticle(ArticleForm form) {
-        System.out.println(form.toString());
-        return "";
+        log.info(form.toString());
+
+       // 1. Dto를 변환! Entity!
+        Article article = form.toEntity();
+        log.info(article.toString());
+
+        // 2. Repository에게 Entity를 DB안에 저장하게 함!
+        Article saved = articleRepository.save(article);
+        log.info(saved.toString());
+
+        return "redirect:/articles/" + saved.getId();
     }
+
+    @GetMapping("/articles/{id}")
+    public String show(@PathVariable Long id, Model model) {
+        log.info("id = {}", id);
+
+        // 1. id로 데이터를 가져옴!
+        Article articleEntity = articleRepository.findById(id).orElse(null);
+
+        // 2. 가져온 데이터를 모델에 등록!
+        model.addAttribute("article", articleEntity);
+
+        // 3. 보여줄 페이지를 설정!
+        return "articles/show";
+    }
+
+    @GetMapping("/articles")
+    public String index(Model model) {
+        // 1: 모든 Article을 가져온다!
+        List<Article> articleEntityList = articleRepository.findAll();
+
+        // 2: 가져온 Article 묶음을 뷰로 전달!
+        model.addAttribute("articleList", articleEntityList);
+        // 3: 뷰 페이지를 설정!
+        return "articles/index"; // articles/index.mustache
+    }
+
+    @GetMapping("/articles/{id}/edit")
+    public String edit(@PathVariable Long id, Model model) {
+        // 수정할 데이터를 가져오기!
+        Article articleEntity = articleRepository.findById(id).orElse(null);
+
+        // 모델에 데이터를 등록!
+        model.addAttribute("article", articleEntity);
+
+        // 뷰 페이지 설정
+        return "articles/edit";
+    }
+
 
 }
---
+++ src/main/java/com/example/hongproject/entity/Article.java
@@ -0,0 +1,32 @@
+package com.example.hongproject.entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity // DB가 해당 객체를 인식 가능!
+@AllArgsConstructor
+@NoArgsConstructor // 디폴트 생성자를 추가!
+@ToString
+@Getter
+public class Article {
+
+    @Id // 대표값을 지정! like a 주민등록번호
+    @GeneratedValue // 1, 2, 3, ... 자동 생성 어노테이션!
+    private Long id;
+
+    @Column
+    private String title;
+
+    @Column
+    private String content;
+
+
+}
